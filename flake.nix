6db363bfd11d138776c536dbfdfc00c566970f8b
ElrohirGT <elrohirgt@gmail.com>
2026-03-16 21:01:50 -0600

    feat: Added flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A basic multiplatform flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          go
+          libGL
+          libX11
+          libxrandr
+          libxcursor
+          libxinerama
+          libxi
+          libxxf86vm
+        ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.wayland}/lib:${pkgs.lib.getLib pkgs.libGL}/lib:${pkgs.lib.getLib pkgs.libGL}/lib:$LD_LIBRARY_PATH
+        '';
+      };
+    });
+  };
+}
